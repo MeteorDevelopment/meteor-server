ecbf25056c2b56144c5231727ae890edc562faa1
seasnail8169 <seasnail@meteorclient.com>
2021-04-16 02:40:09 +0100

    More website stuff + animated and updating download counter

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,34 @@
+
 httpGet("/api/stats").then(res => {
     $("#version").textContent += res.version
     $("#mc-version").textContent += res.mcVersion
-    $("#downloads").textContent += res.downloads
+    $("#counter").textContent += 0
 })
+
+let target = 1;
+let current = 0;
+
+httpGet("/api/stats").then(res => {
+    target = +res.downloads
+})
+
+const updateDownloads = () => {
+    httpGet("/api/stats").then(res => {
+        $("#counter").textContent = `Downloads: ${res.downloads}`
+    })
+
+    setTimeout(updateDownloads, 30000);
+}
+
+const animateDownloads = () => {
+    if (current < target) {
+        current += target / 170;
+        $("#counter").innerText = `Downloads: ${Math.ceil(current)}`;
+        setTimeout(animateDownloads, 1);
+    } else {
+        $("#counter").innerText = `Downloads: ${Math.ceil(target)}`;
+        updateDownloads()
+    }
+}
+
+animateDownloads()
