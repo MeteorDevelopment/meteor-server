78e7ac4d2ac85a29dee84c14c4027363a72f9c95
MineGame159 <petulko08@gmail.com>
2021-05-31 14:20:15 +0200

    Forgot

--- static/js/index.js
+++ static/js/index.js
@@ -54,32 +54,5 @@ const animatePlayers = () => {
     }
 }
 
-const navSlide = () => {
-    const burger = $(".burger")
-    const nav = $(".nav-links")
-    const navLinks = document.querySelectorAll(".nav-links li")
-
-    //Toggle borgor
-    burger.addEventListener("click", () => {
-        nav.classList.toggle("nav-active")
-
-        //Animate
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ``
-            }
-            else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${
-                    index / 7 + 0.1
-                }s`
-            }
-        })
-
-        //Animate da borgor
-        burger.classList.toggle("toggle");
-    })
-}
-
 animateDownloads()
 animatePlayers()
-navSlide();
--- static/js/navbar.js
+++ static/js/navbar.js
@@ -1,19 +1,27 @@
-const burger = $(".burger")
-const list = $("nav ul")
-const items = document.querySelectorAll("nav ul li")
+const navSlide = () => {
+    const burger = $(".burger")
+    const nav = $(".nav-links")
+    const navLinks = document.querySelectorAll(".nav-links li")
 
-burger.addEventListener("click", () => {
-    list.classList.toggle("active")
+    // Toggle borgor
+    burger.addEventListener("click", () => {
+        nav.classList.toggle("nav-active")
 
-    items.forEach((link, index) => {
-        if (link.style.animation) {
-            link.style.animation = ``
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${
-                index / 7 + 0.1
-            }s`
-        }
-    });
+        // Animate
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = ``
+            }
+            else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${
+                    index / 7 + 0.1
+                }s`
+            }
+        })
 
-    burger.classList.toggle("active")
-});
+        // Animate da borgor
+        burger.classList.toggle("toggle");
+    })
+}
+
+navSlide()
