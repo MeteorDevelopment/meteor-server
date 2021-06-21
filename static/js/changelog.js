f0a1da0cc62ceea428f08377918312a430f08047
MineGame159 <petulko08@gmail.com>
2021-06-21 18:50:13 +0200

    Fixed changelog page

--- static/js/changelog.js
+++ static/js/changelog.js
@@ -1,7 +1,4 @@
 httpGet("/api/stats").then(res => {
-    $("#version").textContent = res.version
-    document.querySelectorAll(".mc-version").forEach(element => element.textContent = res.mcVersion)
-
     let ul = $("#changelog")
 
     for (let i in res.changelog) {
