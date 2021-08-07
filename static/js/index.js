eaea8ae4da14118e3dae7588e8b8aa1980a83e6b
MineGame159 <petulko08@gmail.com>
2021-08-07 14:48:05 +0200

    A

--- static/js/index.js
+++ static/js/index.js
@@ -1,59 +1,17 @@
-let targetDownloads = 1
-let currentDownloads = 0
-
-let targetPlayers = 1
-let currentPlayers = 0
-
 httpGet("/api/stats").then(res => {
     $("#version").textContent = res.version
     $("#dev-version").textContent = res.dev_build_version + " - " + res.devBuild
-    $("#mc-version").textContent = res.mcVersion
-
-    targetDownloads = res.downloads
-    targetPlayers = res.onlinePlayers
+    $("#downloads").textContent = res.downloads
+    $("#online-players").textContent = res.onlinePlayers
 })
 
-const updateDownloads = () => {
+const updateStats = () => {
     httpGet("/api/stats").then(res => {
-        $("#downloads").textContent = `${res.downloads}`
+        $("#downloads").textContent = res.downloads
+        $("#online-players").textContent = res.onlinePlayers
     })
 
-    setTimeout(updateDownloads, 30000);
+    setTimeout(updateStats, 30000);
 }
 
-const animateDownloads = () => {
-    if (currentDownloads < targetDownloads) {
-        currentDownloads += targetDownloads / 170;
-
-        $("#downloads").innerText = `${Math.ceil(currentDownloads)}`
-        setTimeout(animateDownloads, 1);
-    }
-    else {
-        $("#downloads").innerText = `${Math.ceil(targetDownloads)}`
-        updateDownloads()
-    }
-}
-
-const updatePlayers = () => {
-    httpGet("/api/stats").then(res => {
-        $("#online-players").textContent = `${res.onlinePlayers}`
-    })
-
-    setTimeout(updatePlayers, 30000);
-}
-
-const animatePlayers = () => {
-    if (currentPlayers < targetPlayers) {
-        currentPlayers += targetPlayers / 170;
-
-        $("#online-players").innerText = `${Math.ceil(currentPlayers)}`
-        setTimeout(animatePlayers, 1);
-    }
-    else {
-        $("#online-players").innerText = `${Math.ceil(targetPlayers)}`
-        updatePlayers()
-    }
-}
-
-animateDownloads()
-animatePlayers()
+setTimeout(updateStats, 30000)
