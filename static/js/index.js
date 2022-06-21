6e8970464b3068efbb18fbb49c77bc577ebb09d2
MineGame159 <petulko08@gmail.com>
2022-06-21 14:03:25 +0200

    Include mc version in download buttons

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 httpGet("/api/stats").then(res => {
-    $("#version").textContent = res.version
-    $("#dev-version").textContent = res.dev_build_version + " - " + res.devBuild
+    $("#version").textContent = res.version + " [" + res.mc_version + "]"
+    $("#dev-version").textContent = res.dev_build_version + " - " + res.devBuild + " [" + res.dev_build_mc_version + "]"
     $("#downloads").textContent = res.downloads
     $("#online-players").textContent = res.onlinePlayers
 })
