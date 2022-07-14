91ead524cb7a072c84d32e3b0abafaab820ab2e3
MineGame159 <petulko08@gmail.com>
2022-07-14 19:52:03 +0200

    CORS 4.0

--- pkg/web/web.go
+++ pkg/web/web.go
@@ -33,6 +33,7 @@ func Cors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
